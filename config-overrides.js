a8f86a3cbe2e847e4c9376b832b0028b0689882e
Yumiko <yumiko.liu@yahoo.com>
2019-05-16 23:56:21 +0800

    配置react-app-rewired

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,15 @@
-const { override, fixBabelImports } = require('customize-cra');
+const { 
+  override,
+  fixBabelImports,
+  addLessLoader,
+  addPostcssPlugins,
+} = require('customize-cra');
 
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd-mobile',
     style: 'css',
-  })
+  }),
+  addLessLoader(),
+  addPostcssPlugins([require('autoprefixer')({ browsers: ['last 10 Chrome versions', 'last 5 Firefox versions', 'Safari >= 6', 'ie > 8'] })])
 );
--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css';
 
 import { Button } from 'antd-mobile';
 
@@ -8,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
