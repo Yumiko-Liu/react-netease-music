9facc180a366a368597c322560269d567c05baa4
Yumiko <yumiko.liu@yahoo.com>
2019-05-20 17:10:11 +0800

    Updates

---
+++ precache-manifest.52f6d5900312c217cf7675a63082a627.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3cfc522009a8037a16ee952d861e79",
+    "url": "/Yumiko-Liu/react-netease-music/index.html"
+  },
+  {
+    "revision": "2386bacb1660f6fd8445",
+    "url": "/Yumiko-Liu/react-netease-music/static/css/main.06682fe3.chunk.css"
+  },
+  {
+    "revision": "4046d5de130cb81d9898",
+    "url": "/Yumiko-Liu/react-netease-music/static/js/2.b54b4d0f.chunk.js"
+  },
+  {
+    "revision": "2386bacb1660f6fd8445",
+    "url": "/Yumiko-Liu/react-netease-music/static/js/main.8e3a43ee.chunk.js"
+  },
+  {
+    "revision": "3ebdcf668f3afff90c83",
+    "url": "/Yumiko-Liu/react-netease-music/static/js/runtime~main.715a54ec.js"
+  }
+]);
